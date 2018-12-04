32cd8d4cb319633af145d41989e709431744279e
Gregory Becker <becker33@llnl.gov>
2018-12-03 17:14:43 -0800

    Added test for multimethod inheritance

---
+++ var/spack/repos/builtin.mock/packages/multimethod-inheritor/package.py
@@ -0,0 +1,18 @@
+# Copyright 2013-2018 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from six import string_types
+
+from spack import *
+import spack.architecture
+
+from spack.pkg.builtin.mock.multimethod import Multimethod
+
+
+class MultimethodInheritor(Multimethod):
+    """This package is designed for use with Spack's multimethod test.
+       It has a bunch of test cases for the @when decorator that the
+       test uses.
+    """
